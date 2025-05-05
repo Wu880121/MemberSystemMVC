fec7547b3bc5c172e35be4dea842bee16c12832c
wuzixin <kocoimur0003@gmail.com>
2025-05-06 00:28:01 +0800

    更新Line第三方問題

--- app/views/pages/ShowLineBlockNotice.php
+++ app/views/pages/ShowLineBlockNotice.php
@@ -21,9 +21,9 @@
     </style>
 </head>
 <body>
-    <h2>⚠️ LINE 不支援 Google 登入</h2>
-    <p class="hint">請點選右上角「⋯」<br>選擇「使用瀏覽器開啟」</p>
-	<h2>或是複製網址</h2>
-	<a href="https://keepgoingpiggy.com">https://keepgoingpiggy.com</a>
+    <h2 style="font-size:20px;">⚠️ LINE 不支援 Google 登入</h2>
+    <p class="hint" style="font-size:20px;">請點選右上角「⋯」<br>選擇「使用瀏覽器開啟」</p>
+	<h2 style="font-size:20px;">或是複製網址</h2>
+	<a href="https://keepgoingpiggy.com"style="font-size:20px;">https://keepgoingpiggy.com</a>
 </body>
 </html>
