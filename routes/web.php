d5c0429d6046526b94aaea870cbe7c81aedad770
wuzixin <kocoimur0003@gmail.com>
2025-05-06 00:20:33 +0800

    更新Line第三方問題

--- routes/web.php
+++ routes/web.php
@@ -147,7 +147,7 @@ switch ($route) {
 		
 		case 'google_login':
 		require_once __DIR__. '/../app/services/GoogleLoginRequestInformation.php';
-		
+		require_once __DIR__ . '/../app/controllers/StaticPageController.php';
 		// 在 router 判斷 google_login 時加入這段
          $userAgent = $_SERVER['HTTP_USER_AGENT'] ?? '';
          if (preg_match('/Line/i', $userAgent)) {
