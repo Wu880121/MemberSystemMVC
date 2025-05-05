290d992814b1524aed78f4ddf8e073529e50e900
wuzixin <kocoimur0003@gmail.com>
2025-05-06 00:35:47 +0800

    更新Line第三方問題

--- app/views/pages/ShowLineBlockNotice.php
+++ app/views/pages/ShowLineBlockNotice.php
@@ -15,15 +15,24 @@
             margin-top: 20px;
         }
         .hint {
-            font-size: 18px;
+            font-size: 40%;
             line-height: 1.6;
         }
+		
+		h2{
+			font-size: 40%;
+		}
+		
+		a{
+			
+			font-size: 40%;
+		}
     </style>
 </head>
 <body>
-    <h2 style="font-size:40%;">⚠️ LINE 不支援 Google 登入</h2>
-    <p class="hint" style="font-size:40%;">請點選右上角「⋯」，或右下角<br>選擇「使用瀏覽器開啟」</p>
-	<h2 style="font-size:40%;">或是複製網址</h2>
-	<a href="https://keepgoingpiggy.com"style="font-size:40%;">https://keepgoingpiggy.com</a>
+    <h2>⚠️ LINE 不支援 Google 登入</h2>
+    <p class="hint">請點選右上角「⋯」，或右下角<br>選擇「使用瀏覽器開啟」</p>
+	<h2>或是複製網址</h2>
+	<a href="https://keepgoingpiggy.com">https://keepgoingpiggy.com</a>
 </body>
 </html>
