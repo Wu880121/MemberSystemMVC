1f4a1a13d8dcd698514e2b6e501b6ddb32e308e3
wuzixin <kocoimur0003@gmail.com>
2025-05-08 14:38:00 +0800

    測試

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Document</title>
+  <title>Eating For You</title>
   <link rel="stylesheet" href="/assets/css/home.css">
     <link rel="stylesheet" href="/assets/css/footer.css">
 	<link rel="stylesheet" href="/assets/css/nav.css">
